3b2ef869e43f72641236fb3cc7e66f753796d1b9
Nikita <Nikita@example.com>
2023-08-14 14:10:25 +0300

    add prod docker conf.

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = env.str('SECRET_KEY', 'django-insecure-oxthbs+oj66i-changeme')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG', False)
 
-ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=['158.160.73.93', '127.0.0.1', 'localhost', 'foodgram56.ddns.net'])
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=['127.0.0.1', 'localhost'])
 
 # Application definition
 
